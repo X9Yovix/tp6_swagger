a4fe2328318154e73aac048e34c8babd3a756271
Karim Ouazzou <karimouazzou@gmail.com>
2023-11-25 08:29:54 +0100

    first commit

---
+++ src/main/java/com/example/tp6_swagger/SpringBootApiDocApplication.java
@@ -0,0 +1,13 @@
+package com.example.tp6_swagger;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootApiDocApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootApiDocApplication.class, args);
+	}
+
+}
