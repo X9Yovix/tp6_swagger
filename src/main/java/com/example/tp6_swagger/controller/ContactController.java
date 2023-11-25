d0b9afd70db65088a75a972d223adb6acd1e9be0
Karim Ouazzou <karimouazzou@gmail.com>
2023-11-25 09:05:55 +0100

    implementation of all methods in contact controller

--- src/main/java/com/example/tp6_swagger/controller/ContactController.java
+++ src/main/java/com/example/tp6_swagger/controller/ContactController.java
@@ -3,13 +3,38 @@ package com.example.tp6_swagger.controller;
 import com.example.tp6_swagger.model.Contact;
 import com.example.tp6_swagger.repository.ContactRepository;
 import lombok.AllArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
 @AllArgsConstructor
 public class ContactController {
+
     private final ContactRepository contactRepository;
+
+    @GetMapping("/contacts")
+    public List<Contact> getAllContacts(){
+        return this.contactRepository.getAll();
+    }
+
+    @GetMapping("/contacts/{id}")
+    public Contact getContactByID(@PathVariable Long id){
+        return this.contactRepository.getByID(id);
+    }
+
+    @PostMapping("/contacts")
+    public Contact saveContact(@RequestBody Contact c){
+        return this.contactRepository.save(c);
+    }
+
+    @PutMapping("/contacts/{id}")
+    public Contact updateContact(@RequestBody Contact c){
+        return this.contactRepository.update(c);
+    }
+
+    @DeleteMapping("/contacts/{id}")
+    public boolean deleteContact(@PathVariable Long id){
+        return this.contactRepository.delete(id);
+    }
 }
